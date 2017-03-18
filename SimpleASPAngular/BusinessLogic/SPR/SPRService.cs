74f5ad8e07a66197c25b517049a836391922fdc5
Malarick <arnold.nodz@gmail.com>
2017-03-18 23:36:43 +0700

    Entity to model method for SPR

--- SimpleASPAngular/BusinessLogic/SPR/SPRService.cs
+++ SimpleASPAngular/BusinessLogic/SPR/SPRService.cs
@@ -15,6 +15,16 @@ namespace SimpleASPAngular.BusinessLogic.SPR
             this.db = db;
         }
 
+        public List<SPRHeaderModel> GetSPRHeaders() {
+            var SPRHeaderModels = new List<SPRHeaderModel>();
+            var SPRHeaders = db.TH_SPR.ToList();
+            SPRHeaders.ForEach(s => {
+                SPRHeaderModels.Add(EntityToHeaderModel(s));
+            });
+
+            return SPRHeaderModels;
+        }
+
         public List<SPRHeaderModel> GetSPRHeaderByFilter()
         {
             var SPRHeaderModels = new List<SPRHeaderModel>();
@@ -117,12 +127,154 @@ namespace SimpleASPAngular.BusinessLogic.SPR
             return true;
         }
 
+        public List<SPRDetailModel> GetDetailsByHeaderId(string id) {
+            var detailModels = new List<SPRDetailModel>();
+            var details = db.TD_SPR
+                .Where(d => d.Kode_Spr == id)
+                .OrderBy(o => o.Kode_Material)
+                .ToList();
+            details.ForEach(d => {
+                detailModels.Add(EntityToDetailModel(d));
+            });
+
+            return detailModels;
+        }
+
+        public bool AddSPRDetail(SPRDetailModel detailModel) {
+            try {
+                var detail = new TD_SPR
+                {
+                    Kode_Spr = detailModel.Kode_Spr,
+                    Kode_Material = detailModel.Kode_Material,
+                    Jenis_Material = detailModel.Jenis_Material,
+                    Merk = detailModel.Merk,
+                    Spesifikasi = detailModel.Spesifikasi,
+                    Keterangan = detailModel.Keterangan,
+                    Volume = detailModel.Volume,
+                    Unit = detailModel.Unit,
+                    Tanggal_Rencana_Terima = detailModel.Tanggal_Rencana_Terima,
+                    Status_Disetujui = detailModel.Status_Disetujui
+                };
+
+                var existing = db.TD_SPR.Find(detailModel.Kode_Spr, detailModel.Kode_Material);
+                if(existing != null) {
+                    return false;
+                }
+
+                db.TD_SPR.Add(detail);
+                db.SaveChanges();
+            } catch (Exception ex) {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool EditSPRDetail(SPRDetailModel detailModel) {
+            try {
+                var detail = db.TD_SPR.Find(detailModel.Kode_Spr, detailModel.Kode_Material);
+                detail.Jenis_Material = detailModel.Jenis_Material;
+                detail.Merk = detailModel.Merk;
+                detail.Spesifikasi = detailModel.Spesifikasi;
+                detail.Keterangan = detailModel.Keterangan;
+                detail.Volume = detailModel.Volume;
+                detail.Unit = detailModel.Unit;
+                detail.Tanggal_Rencana_Terima = detailModel.Tanggal_Rencana_Terima;
+                detail.Status_Disetujui = detailModel.Status_Disetujui;
+
+                db.SaveChanges();
+            } catch (Exception ex) {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool DeleteSPRDetail(SPRDetailModel detailModel) {
+            try {
+                var detail = db.TD_SPR.Find(detailModel.Kode_Spr, detailModel.Kode_Material);
+                db.TD_SPR.Remove(detail);
+                db.SaveChanges();
+            } catch (Exception ex) {
+                return false;
+            }
+            return true;
+        }
+
         private SPRHeaderModel EntityToHeaderModel(TH_SPR spr) {
-            return null;
+            var headerModel = new SPRHeaderModel {
+                Kode_Spr = spr.Kode_Spr,
+                Kode_Proyek = spr.Kode_Proyek,
+                Kode_Zona = spr.Kode_Zona,
+                Tanggal_Spr = spr.Tanggal_Spr,
+                Tujuan_SPR = spr.Tujuan_SPR,
+                Nama_Peminta = spr.Nama_Peminta,
+                Status_Disetujui1 = spr.Status_Disetujui1,
+                Status_Disetujui2 = spr.Status_Disetujui2,
+                Nama_Pembuat = spr.Nama_Pembuat,
+                Nama_Pengubah = spr.Nama_Pengubah,
+                Nama_Penyetuju1 = spr.Nama_Penyetuju1,
+                Nama_Penyetuju2 = spr.Nama_Penyetuju2,
+                Tanggal_Dibuat = spr.Tanggal_Dibuat,
+                Tanggal_Diubah = spr.Tanggal_Diubah,
+                Tanggal_Disetujui1 = spr.Tanggal_Disetujui1,
+                Tanggal_Disetujui2 = spr.Tanggal_Disetujui2
+            };
+
+            headerModel.Details = new List<SPRDetailModel>();
+            spr.Details.ToList().ForEach(d => {
+                var detailModel = new SPRDetailModel {
+                    Kode_Spr = d.Kode_Spr,
+                    Kode_Material = d.Kode_Material,
+                    Jenis_Material = d.Jenis_Material,
+                    Merk = d.Merk,
+                    Spesifikasi = d.Spesifikasi,
+                    Keterangan = d.Keterangan,
+                    Volume = d.Volume,
+                    Unit = d.Unit,
+                    Tanggal_Rencana_Terima = d.Tanggal_Rencana_Terima,
+                    Status_Disetujui = d.Status_Disetujui,
+                    Header = new SPRHeaderModel
+                    {
+                        Kode_Spr = d.Header.Kode_Spr,
+                        Tanggal_Spr = d.Header.Tanggal_Spr
+                    }
+                };
+                
+                if (d.Jenis_Material.Equals("01"))
+                {
+                    detailModel.Nama_Material = db.M_Material_CC.Find(d.Kode_Material).Deskripsi;
+                }
+                else
+                {
+                    detailModel.Nama_Material = db.M_Material_NonPokok.Find(d.Kode_Material, spr.Kode_Proyek).Deskripsi;
+                }
+
+                headerModel.Details.Add(detailModel);
+            });
+            
+            return headerModel;
         }
 
         private SPRDetailModel EntityToDetailModel(TD_SPR spr) {
-            return null;
+            var detailModel = new SPRDetailModel{
+                Kode_Spr = spr.Kode_Spr,
+                Kode_Material = spr.Kode_Material,
+                Jenis_Material = spr.Jenis_Material,
+                Merk = spr.Merk,
+                Spesifikasi = spr.Spesifikasi,
+                Keterangan = spr.Keterangan,
+                Volume = spr.Volume,
+                Unit = spr.Unit,
+                Tanggal_Rencana_Terima = spr.Tanggal_Rencana_Terima,
+                Status_Disetujui = spr.Status_Disetujui,
+                Header = new SPRHeaderModel {
+                    Kode_Spr = spr.Header.Kode_Spr,
+                    Tanggal_Spr = spr.Header.Tanggal_Spr
+                }
+            };
+
+            return detailModel;
         }
     }
 }
